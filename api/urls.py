908c1be268c6551c53c58e71f09ac6071039049d
kjm4027 <gameliker16@naver.com>
2023-05-10 23:41:29 +0900

    1st

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# here we are importing all the Views from the views.py file
+from . import views
+
+# a list of all the urls
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new_chat/', views.new_chat, name='new_chat'),
+    path('error-handler/', views.error_handler, name='error_handler'),
+]
